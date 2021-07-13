62afeda8cd446fde95790ea20a29144f8569ae6a
a-santiago <santiago.62596@gmail.com>
2021-07-13 11:39:05 -0400

    commit1.0.1
    Footer have been fixed for all sites and code has been extended to display the correct error messages

--- js/site.js
+++ js/site.js
@@ -6,9 +6,40 @@ function getNumbers() {
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
+    // The above lines can be used in a parseInt
+    //parseFloat is a decimal function
+    //while parseInt is an interger function 
+    //The example below this line is the parseInt example 
+    let newStart = parseInt(startValue)
+    let newEnd = parseInt(endValue)
+
+    let errorState = false;
+    let errorMsg = "";
+
+    if (isNaN(newStart) || isNaN(newEnd)) {
+        errorState = true;
+        errorMsg += "Please use numbers<hr/>"
+    }
+
+    if (newStart > newEnd) {
+        errorState = true;
+        errorMsg += "Start value must be less than end value<hr/>"
+    }
+    if (newStart > 10000 || newStart < -10000 || newEnd > 10000 || newEnd < -10000) {
+        errorState = true;
+        errorMsg += "-10,000 and 10,000 are the limit<hr/>"
+    }
+
+    if (errorState) {
+        Swal.fire(
+            'Uh Oh!',
+            'Something went wrong',
+            'Error'
+        )
+    }
     //take the start and end values as the bounds fro a "for" loop
     // that will create an array and return it
-    let numbers = generateNumbers(startValue, endValue);
+    let numbers = generateNumbers(newStart, newEnd);
 
     //take the array that was returned and pass it to a function that will
     //display it
@@ -18,11 +49,11 @@ function getNumbers() {
 }
 
 //Wrapper Function/method - a funtion that calls other functions
-function generateNumbers(startValue, endValue) {
+function generateNumbers(newStart, newEnd) {
     let numbers = [];
 
     //loop over number from startValue to endValue
-    for (let index = startValue; index <= endValue; index++) {
+    for (let index = newStart; index <= newEnd; index++) {
         numbers.push(index);
     }
 
